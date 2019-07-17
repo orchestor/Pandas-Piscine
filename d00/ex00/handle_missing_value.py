016be8496a06adbfc1511ba34536e12d78ebec00
Lin <Lin@2x.local>
2019-07-17 10:08:17 -0700

    handle space

--- d00/ex00/handle_missing_value.py
+++ d00/ex00/handle_missing_value.py
@@ -6,7 +6,12 @@ csv_data = \
        1.0, 2.0, 3.0, 4.0
 5.0, 6.0, , 8.0
 10,11,0.23 '''
-df = pd.read_csv(StringIO(csv_data))
+
+# Handle space with skipinitialspace = True option
+df = pd.read_csv(StringIO(csv_data), skipinitialspace=True)
 print(df)
 stats_df = df.isnull().sum()
 print(stats_df)
+
+# Convert to numpy object
+print(df.values)
